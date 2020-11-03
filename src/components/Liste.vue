35b6cd5ccad6fa7733ed22dc2c12987bed3de46c
Ethifinance <j.boyer69003@gmail.com>
2020-11-03 12:17:18 +0100

    first push

---
+++ src/components/Liste.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Liste de songs</h3>
+    <ul>
+      <li>Song #1: Dirty Dancing</li>
+      <li>Song #2: Animals</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // VueJS
+};
+</script>
